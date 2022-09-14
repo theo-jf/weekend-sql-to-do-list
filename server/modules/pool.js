c571a1348bde38596f49e7287a7c04fa0fb24be9
Theo Janke-Furman <tjankefurman@gmail.com>
2022-09-13 21:16:32 -0500

    ready for heroku deploy

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,21 +1,30 @@
 const pg = require('pg');
+const url = require('url');
 
-const config = {
-  database: 'weekend-to-do-app', 
-  host: 'localhost', 
-  port: 5432, 
-  max: 10, 
-  idleTimeoutMillis: 30000 
-};
+let config = {};
+
+if (process.env.DATABASE_URL) {
+  config = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  };
+} else {
+  config = {
+    host: 'localhost',
+    port: 5432,
+    database: 'weekend-to-do-app'
+  };
+}
 
 const pool = new pg.Pool(config);
 
-pool.on("connect", () => {
-  console.log("connected to postgres");
+pool.on('connect', () => {
+  console.log('Postgesql connected');
 });
 
-pool.on("error", (err) => {
-  console.log("error connecting to postgres", err);
+pool.on('error', (err) => {
+  console.log('Unexpected error on idle client', err);
+  process.exit(-1);
 });
 
 module.exports = pool;
